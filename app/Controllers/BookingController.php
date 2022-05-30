5f2ddb54fda902a905bd5169484500fb6f87e859
harunasrori <harunasrori407@gmail.com>
2022-05-30 14:52:27 +0700

    progressing on booking

--- app/Controllers/BookingController.php
+++ app/Controllers/BookingController.php
@@ -2,3 +2,14 @@
 
 namespace Kel1\ProjekAkhirPemweb\Controllers;
 use Kel1\ProjekAkhirPemweb\Models\Booking;
+
+class BookingController extends Controller{
+
+    public function check_availability()
+    {
+        $this->view('templates/header');
+        $this->view('index');
+        $this->view('templates/footer');
+    }
+
+}
--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('BASEURL', '../../public');
 
 // Database
-// define('DB_HOST' , 'localhost');
-// define('DB_USER' , 'root');
-// define('DB_PASS' , '1234567');
-// define('DB_NAME' , '');
+define('DB_HOST' , 'localhost');
+define('DB_USER' , 'root');
+define('DB_PASS' , '1234567');
+define('DB_NAME' , 'petmate');
