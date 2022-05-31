d070bd64b87d58ad45ef29d0ae9d428b7c105d37
ptri16 <ptrirahma16@gmail.com>
2022-05-31 08:27:41 +0700

    add part of userControl

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -1,13 +1,42 @@
 <?php
 
-namespace Kel1\ProjekAkhirPemweb\Models;
+namespace Kel1\ProjekAkhirPemweb\models;
 
-class User{
+use Database;
+use PDO;
+use PDOException;
 
-    public function addUser(Type $var = null)
-    {
-       
+class User_model{
+
+    private $table = 'pengguna';
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
     }
 
+    // public function __destruct() {
+    //     unset($this->db);
+    // }
+
+    public function getNama()
+    {
+        $this->db->query("SELECT nama FROM pengguna WHERE nama = 'chii' " );
+        return $this->db->resultSet();
+    }
+
+    public function addUser($data)
+    {
+        $nama = $data['nama'];
+        $username = $data['username'];
+        $email = $data['email'];
+        $alamat = $data['alamat'];
+        $pwd = $data['password'];
+        $que = ("INSERT INTO pengguna  VALUES (
+            NULL, '$nama','$username', '$email', '$alamat', '$pwd')");
+
+        $this->db->query($que);
+        $this->db->execute();
+    }
 
 }
