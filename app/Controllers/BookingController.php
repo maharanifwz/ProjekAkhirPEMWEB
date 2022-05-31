e9f398e72f238191a0e666573586116d303e9670
harunasrori <harunasrori407@gmail.com>
2022-05-31 19:07:28 +0700

    progressing on booking -form

--- app/Controllers/BookingController.php
+++ app/Controllers/BookingController.php
@@ -16,17 +16,14 @@ class BookingController extends Controller
     {
         $booking_model = new Booking_model();
 
-        // $data['jam'] = $this->model('Booking_model')->getHour();
         $data['available_hour'] = $booking_model->getAvailableHour($_GET["date"], $_GET["jumlahHewan"]);
         $this->show('detailClinic2', $data);
     }
 
     public function fill_data()
     {
-        #belom mulai bikin
         $booking_model = new Booking_model();
 
-        // $data['jam'] = $this->model('Booking_model')->getHour();
         $this->show('form');
     }
 }
