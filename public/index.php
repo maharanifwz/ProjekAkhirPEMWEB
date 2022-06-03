887f47ea548652cdfdd07f24d3c89f7491795d7e
harunasrori <harunasrori407@gmail.com>
2022-06-03 21:37:18 +0700

    progressing on consultation history

--- public/index.php
+++ public/index.php
@@ -15,7 +15,6 @@ Routes::add('GET','/tentangKami', HomeController::class,'tentangKami');
 Routes::add('GET','/form', HomeController::class,'form');
 Routes::add('GET','/form2', HomeController::class,'form2');
 Routes::add('GET','/form3', HomeController::class,'form3');
-Routes::add('GET','/ShowAllHistory', RiwayatController::class,'ShowAllHistory');
 
 // User
 Routes::add('GET','/login', HomeController::class,'login');
@@ -23,6 +22,9 @@ Routes::add('POST','/prosesLogin', UserController::class,'login');
 Routes::add('GET','/logout', UserController::class,'logout');
 Routes::add('GET','/register', HomeController::class,'register');
 Routes::add('POST','/prosesRegister', UserController::class,'register');
+
+// User - Riwayat Rekomendasi
+Routes::add('GET','/ShowAllHistory', RiwayatController::class,'ShowAllHistory');
 
 
 // Booking
