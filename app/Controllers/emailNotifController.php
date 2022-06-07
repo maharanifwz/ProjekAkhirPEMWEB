ac4a53abce5296119fa343604071fbf184572476
harunasrori <harunasrori407@gmail.com>
2022-06-07 14:32:21 +0700

    progressing on emailnotificiation

--- app/Controllers/emailNotifController.php
+++ app/Controllers/emailNotifController.php
@@ -3,38 +3,43 @@
 namespace Kel1\ProjekAkhirPemweb\Controllers;
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require_once "../../vendor/autoload.php";
 
-//PHPMailer Object
-$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
-
-//From email address and name
-$mail->From = "from@yourdomain.com";
-$mail->FromName = "Full Name";
-
-//To address and name
-$mail->addAddress("recepient1@example.com", "Recepient Name");
-$mail->addAddress("recepient1@example.com"); //Recipient name is optional
-
-//Address to which recipient will reply
-$mail->addReplyTo("reply@yourdomain.com", "Reply");
-
-//CC and BCC
-$mail->addCC("cc@example.com");
-$mail->addBCC("bcc@example.com");
-
-//Send HTML or Plain Text email
-$mail->isHTML(true);
-
-$mail->Subject = "Subject Text";
-$mail->Body = "<i>Mail body in HTML</i>";
-$mail->AltBody = "This is the plain text version of the email content";
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 try {
+    //Server settings
+    $mail->SMTPDebug = 1;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'harunasrori408@gmail.com';                     //SMTP username
+    $mail->Password   = 'ygoqiinwqdvqoaeq';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = "587";                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('harunasrori408@gmail.com', 'petmate');
+    $mail->addAddress('maharanifwz@student.ub.ac.id');     //Add a recipient
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    $body = "<p> <strong>HELLO</strong> This is my first Email </p>";
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'This is a test Email';
+
+    $mail->Body    = $body;
+    $mail->AltBody = strip_tags($body);
+
     $mail->send();
-    echo "Message has been sent successfully";
+    echo 'Message has been sent';
 } catch (Exception $e) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
